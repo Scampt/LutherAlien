686f2b09187f48fb9ea412313ea6a00d77e405a0
cd <falcony45@gmail.com>
2022-01-12 18:51:12 -0600

    Fondos agregados, borrador de luthers, y lista para animar

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const boton = document.getElementById("color-btn");
+boton.addEventListener("click", colorChange)
+
+function colorChange() 
+{
+    document.body.classList.toggle("dark")
+    boton.classList.toggle("active");
+}
