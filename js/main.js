2c044f5a52fdcc0901efc73ab468307f3f7e4c7b
cd <falcony45@gmail.com>
2022-02-03 00:58:12 -0600

    Pagina actualizada, casi lista para opensea

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const boton = document.getElementById("color-btn");
-boton.addEventListener("click", colorChange)
-
-function colorChange() 
-{
-    document.body.classList.toggle("dark")
-    boton.classList.toggle("actived");
-}
